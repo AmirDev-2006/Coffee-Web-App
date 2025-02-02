5ff3caf58b351e5ebb2c6ae4512e752963559751
AmirDev-2006 <amir1.kh2006@gmail.com>
2025-02-02 13:50:36 +0330

    complete project

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "brown" : {
+          100:"#ECE0D1",
+          300:"#DBC1AC",
+          600:"#967259",
+          900:"#634832"
+        },
+        "black" : {
+          100:"rgba(0,0,0,0.6)"
+        },
+        "gold" : {
+          100: "#FED7AA"
+        },
+        "grey" :{
+          100 : "#F5F5F5"
+        }
+      },
+      boxShadow:{
+        "Shodow-normal" : "0px 1px 10px rgba(0,0,0,0.05)"
+      },
+      borderRadius:{
+        "4xl" : "2rem"
+      },
+      fontFamily:{
+        "Dana":"Dana",
+        "DanaDemiBold":"Dana DemiBold",
+      },
+      letterSpacing :{
+        "tightest" : '-0.065em'
+      }
+    },
+  },
+  plugins: [],
+}
+
